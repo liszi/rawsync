a53bc24920dc2fbfb1be5d8edf71202eb1439694
Bence Liszkai <bence.liszkai@utopusinsights.com>
2023-11-12 23:49:23 +0100

    Implement rawpack

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Cross platform CLI tool for synchronizing raw and jpeg photos between two folder
 
 ## Usage
 
+### rawsync command
+
 Enter jpeg folder, and execute `rawsync` command. It'll look for a raw folder next to the jpegs, and find
 jpeg-raw pairs. After that it'll ask if you want to move pairless jpegs and pairless raws to trash.
+
+### rawpack command
+
+Enter a folder with mixed jpegs and raw files and execute `rawpack`. The tool will try to sort raw files to
+a `raw` folder, after asking. 
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from send2trash import send2trash
 jpeg_extensions = {'jpg', 'jpeg', 'JPG', 'JPEG'}
 raw_extensions = {'arw', 'ARW'}
 raw_folders = {'raw', 'RAW'}
+default_raw_folder = 'raw'
+
+assert default_raw_folder in raw_folders
 
 
 def find_raw_sub_folder_path(path) -> Optional[str]:
@@ -13,6 +16,10 @@ def find_raw_sub_folder_path(path) -> Optional[str]:
     potential_raw_folders = folders.intersection(raw_folders)
     assert len(potential_raw_folders) < 2, f'Multiple potential raw folders has been found: {potential_raw_folders}'
     return os.path.join(path, next(iter(potential_raw_folders))) if potential_raw_folders else None
+
+
+def find_raw_folder_path(path) -> Optional[str]:
+    return find_raw_sub_folder_path(path) or find_raw_sub_folder_path(os.path.dirname(path))
 
 
 def has_extension(path, extensions: Set) -> bool:
@@ -32,9 +39,10 @@ def find_pairless_raws_and_jpegs(jpeg_files, raw_files) -> Tuple[List[str], List
     return list(raw_key_to_file[key] for key in pairless_raw), list(jpeg_key_to_file[key] for key in pairless_jpeg)
 
 
-def main():
+def raw_sync():
     print('Searching for raw folder...')
     raw_folder = find_raw_sub_folder_path(os.getcwd()) or find_raw_sub_folder_path(os.path.dirname(os.getcwd()))
+    assert raw_folder, f'Can not find raw folder in the working directory with any of the next names:  {raw_folders}'
     jpg_folder = os.getcwd()
     print(f'Raw folder: {raw_folder}')
     print(f'JPG folder: {jpg_folder}')
@@ -59,5 +67,36 @@ def main():
             for file in pairless_jpegs:
                 print(f'Moving {os.path.join(jpg_folder, file)} to trash')
                 send2trash(os.path.join(jpg_folder, file))
+
+    print('All done, exit.')
+
+
+def raw_pack():
+    print('Analizing working directory')
+    workdir = os.getcwd()
+
+    jpegs = [file for file in os.listdir(workdir) if has_extension(file, jpeg_extensions)]
+    raws = [file for file in os.listdir(workdir) if has_extension(file, raw_extensions)]
+
+    assert jpegs, 'No jpeg files has found, nothing to sort.'
+    print(f'Found {len(jpegs)} jpegs and {len(raws)} raw files in the working directory.')
+
+    raw_folder = find_raw_folder_path(workdir)
+    print(f'Found the next potential raw folder: {raw_folder}' if raw_folder else
+          'No potential raw folder has been found.')
+
+    pairless_raws, pairless_jpegs = find_pairless_raws_and_jpegs(jpegs, raws)
+    print(f'Pairless raw file(s) (without jpeg pair): {pairless_raws}')
+    print(f'Pairless jpeg file(s) (without raw pair): {pairless_jpegs}')
+
+    raw_folder = raw_folder or default_raw_folder
+    if raws:
+        if input(f'Do you want to move {len(raws)} raw files to {raw_folder} folder?') in {'y', 'yes'}:
+            os.makedirs(raw_folder, exist_ok=True)
+            for file in raws:
+                source = os.path.join(workdir, file)
+                target = os.path.join(raw_folder, file)
+                print(f'Moving {source} to {raw_folder}')
+                os.replace(source, target)
 
     print('All done, exit.')
